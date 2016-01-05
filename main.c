de784c0e610901415ec8aa01d65fa73bd4c0100e
F74026153bck <nick.chi@gmail.com>
2016-01-05 17:39:36 +0800

    final commit

--- main.c
+++ main.c
@@ -56,11 +56,11 @@ void reset()
         list[i].start=0;
         list[i].end=0;
         list[i].early=0;
-        list[i].late=0;
+        list[i].late=99999;
         list[i].slack=0;
         list[i].value=0;
         node_list[i].early=0;
-        node_list[i].late=0;
+        node_list[i].late=99999;
     }
 }
 
@@ -95,7 +95,7 @@ void calculation()
 {
     int i,j,current_Act;
     current_Act=0;
-    //first decide the early time table
+    //first calculate the early time table
     for(i=0;i<Node_count;i++)
     {
         for(j=0;j<Node_count;j++)   //scan through array
@@ -105,26 +105,79 @@ void calculation()
                 if(node_list[j].early<node_list[i].early+list[current_Act].value)
                 {
                     node_list[j].early=node_list[i].early+list[current_Act].value;
-
                 }
                 list[current_Act].early=node_list[i].early;
                 current_Act++;
             }
         }
     }
+    //then calculate the late time table
+    current_Act=Activity_count;
+    node_list[Node_count-1].late=node_list[Node_count-1].early;
+    for(i=Node_count-1;i>=0;i--)
+    {
+        for(j=Node_count-1;j>=0;j--)
+        {
+            if(raw[i][j]!=0)
+            {
+                current_Act--;
+                if(node_list[i].late>node_list[j].late-raw[i][j])
+                {
+                    node_list[i].late=node_list[j].late-raw[i][j];
+                }
+                list[current_Act].late=node_list[i].late;
+            }
+
+        }
+    }
+    for(i=0;i<Activity_count;i++)
+    {
+        list[i].slack=list[i].late-list[i].early;
+    }
+
 
 }
 
 void output()
 {
-    int i,j;
-    for(i=0;i<Node_count;i++)
+    int i,j,k;
+    k=0;
+    //for(i=0;i<Node_count;i++)
+    //{
+        //printf("%d %d\n",node_list[i].early,node_list[i].late);
+    //}
+    //printf("\n");
+
+    printf("activity |start|  end|slack|\n");
+
+    for(i=0;i<Activity_count;i++)
     {
-        printf("%d ",node_list[i].early);
+        printf("(%3d,%3d)|  %3d|  %3d|  %3d|\n",list[i].start,list[i].end,list[i].early,list[i].late,list[i].slack);
     }
     printf("\n");
     for(i=0;i<Activity_count;i++)
     {
-        printf("%d ",list[i].early);
+        if(list[i].slack==0)
+        {
+            printf("A%d",i+1);
+            if(i!=Activity_count-1)
+            {
+                printf(",");
+            }
+            k++;
+        }
+
+    }
+    if(k==0)
+    {
+        printf("No Critical Path in this AOE network\n");
+    }
+    if(k==1)
+    {
+        printf(" is Critical Path.\n");
+    }
+    if(k>1)
+    {
+        printf(" are Critical Paths.\n");
     }
 }
---
+++ readme_AOE.txt
@@ -0,0 +1,6 @@
+F74026153 紀孟辰 HW7
+
+AOE_network 使用說明
+
+執行後輸入測資檔名後就會輸出所有activity的early 以及 late 和差值(late-early)
+還有所有的critical path
